e2e9273799ceb28106707d2f318ee0f5b7500a4f
Vasiliy <vasja930403@yandex.ru>
2015-09-07 10:56:04 +0300

    guzzle

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,8 +10,8 @@ class HomeController extends Controller {
     public function getIndex() {
         return view('partials.home');
         
-        
-        var_dump(\Illuminate\Contracts\Logging\Log::info('Вот кое-какая полезная информация.'));
+        \Illuminate\Support\Facades\Storage::disk('local')->put('file.txt', 'Contents');
+        Storage::disk('local')->put('file.txt', 'Contents');
 //        Log::info('Вот кое-какая полезная информация.');
 //
 //        Log::warning('Что-то может идти не так.');
